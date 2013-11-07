fb01829d0cf4012082c5e3acb91d66b98a056ea8
doquanghung212 <doquanghung212@gmail.com>
2013-11-07 20:38:02 +0700

    add function getAuthor

--- components/com_products/tem/detail.php
+++ components/com_products/tem/detail.php
@@ -56,7 +56,7 @@ if(isset($_GET['id']))
 			<li><a href="#tabs-2">Book Reference</a></li>
 		</ul>
 		<div id="tabs-1">
-			<p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+			<?php $obj->getAuthor($author_id); ?>
 		</div>
 		<div id="tabs-2">
 			<?php $obj->GetListPro("AND `author_id`='$author_id'",'ORDER BY `cdate` DESC ','LIMIT 0,20');?>
